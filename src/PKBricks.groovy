80e8888f165bc54c5897b0eeceb29372a9ff6581
edalquist <eric.dalquist@gmail.com>
2011-07-19 14:15:45 -0500

    Work on s3 integration

--- src/PKBricks.groovy
+++ src/PKBricks.groovy
@@ -1,35 +1,65 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1' )
 @Grab(group='log4j', module='log4j', version='1.2.16' )
 @Grab(group='commons-lang', module='commons-lang', version='2.6')
+@Grab(group='commons-io', module='commons-io', version='2.0.1')
+@Grab(group='net.java.dev.jets3t', module='jets3t', version='0.8.1')
 
 import org.apache.commons.lang.*;
+import org.apache.commons.io.*;
 import groovy.json.*;
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.Method.GET
 import static groovyx.net.http.ContentType.TEXT
 import static groovyx.net.http.ContentType.HTML
 import static groovyx.net.http.ContentType.URLENC
+import org.jets3t.service.impl.rest.httpclient.RestS3Service
+import org.jets3t.service.security.AWSCredentials
+import org.jets3t.service.model.*
 
+final def historyFileName = "history.json";
+final def brickStatusDefaultsFileName = "brickStatusDefaults.json";
+final def encoding = "UTF-8";
 
 //Create a JSON parser
 def jsonSlurper = new JsonSlurper();
 
+def scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent;
+def config = jsonSlurper.parseText(new File(scriptDir, "config.json").getText(encoding));
+
 //Create a Calendar that corresponds to the Sunday of this week
 def cal = Calendar.getInstance();
-cal.set(Calendar.HOUR, 0);
+cal.set(Calendar.HOUR_OF_DAY, 0);
 cal.set(Calendar.MINUTE, 0);
 cal.set(Calendar.SECOND, 0);
 cal.set(Calendar.MILLISECOND, 0);
 cal.add(Calendar.DAY_OF_YEAR, -1 * Math.max(0, cal.get(Calendar.DAY_OF_WEEK) - 1));
 def brickTimeStamp = cal.getTimeInMillis().toString();
 
-def dataFolder = new File("/Users/edalquist/java/workspace_37/PKBricks/src/");
-def historyFile = new File(dataFolder, "history.json");
-def brickDefaultsFile = new File(dataFolder, "brickStatusDefaults.json");
+//Setup temp dir for json manipulation
+def dataFolder = File.createTempFile("PKBricks.", ".registry");
+dataFolder.delete();
+dataFolder.mkdir();
+
+
+def awsLogin = new AWSCredentials( config.S3.accessKey, config.S3.secretKey )
+def s3 = new RestS3Service( awsLogin )
+
+def historyFile = new File(dataFolder, historyFileName);
+
+if (s3.isObjectInBucket(config.S3.bucketName, historyFileName)) {
+    def historyS3Obj = s3.getObject(config.S3.bucketName, historyFileName);
+    historyS3Stream = historyS3Obj.getDataInputStream();
+    historyFileStream = new FileOutputStream(historyFile);
+    IOUtils.copy(historyS3Obj.getDataInputStream(), historyFileStream);
+    IOUtils.closeQuietly(historyS3Stream);
+    IOUtils.closeQuietly(historyFileStream);
+}
+
+def brickDefaultsFile = new File(scriptDir, brickStatusDefaultsFileName);
 
 def history;
 if (historyFile.exists()) {
-    history = jsonSlurper.parseText(historyFile.getText("UTF-8"));
+    history = jsonSlurper.parseText(historyFile.getText(encoding));
 }
 else {
     history = [:];
@@ -46,12 +76,12 @@ if (brickDataFilename == null) {
 
     brickDataFile = new File(dataFolder, brickDataFilename);
     println "No existing brick data in history for " + brickTimeStamp + ". Using: " + brickDefaultsFile;
-    brickData = jsonSlurper.parseText(brickDefaultsFile.getText("UTF-8"));
+    brickData = jsonSlurper.parseText(brickDefaultsFile.getText(encoding));
     history[brickTimeStamp] = brickDataFilename;
 }
 else {
     brickDataFile = new File(dataFolder, brickDataFilename);
-    brickData = jsonSlurper.parseText(brickDataFile.getText("UTF-8"));
+    brickData = jsonSlurper.parseText(brickDataFile.getText(encoding));
 }
 
 //Parse out levels to point to group names
@@ -91,8 +121,8 @@ loginForm.depthFirst().grep({"INPUT".equalsIgnoreCase(it.name())}).each {
 };
 
 //Add credentials to post body
-loginPostBody['ctl00$MainContent$CharacterName']="TnGoat";
-loginPostBody['ctl00$MainContent$Password']="drick17";
+loginPostBody['ctl00$MainContent$CharacterName']=config.PK.username;
+loginPostBody['ctl00$MainContent$Password']=config.PK.password;
 
 //Submit login
 http.post(path: '/' + loginPath, body: loginPostBody, requestContentType: URLENC );
@@ -124,14 +154,16 @@ memberTable.depthFirst().grep({"tr".equalsIgnoreCase(it.name())}).each {
     //See if the player exists in the data file, if not we need to look up their level
     def playerGroup = brickData.playerGroupLookup[playerName];
     def playerLevel;
-    if (playerGroup == null) {
+    if (playerGroup == null || brickData.groups[playerGroup][playerName] == null) {
         def profileUrl = firstCell['@href'].text();
         def profilePage = http.get(uri : 'http://www.parallelkingdom.com/' + profileUrl);
         def profileLevel = profilePage.depthFirst().grep({ "span".equalsIgnoreCase(it.name()) && it.@id == "MainContent_ProfileBody_character_level"})[0];
         playerLevel = Integer.parseInt(profileLevel.text());
     
-        playerGroup = brickGroups[playerLevel];
-        brickData.playerGroupLookup[playerName] = playerGroup;
+        if (playerGroup == null) {
+            playerGroup = brickGroups[playerLevel];
+            brickData.playerGroupLookup[playerName] = playerGroup;
+        }
         println playerName + ", " + playerLevel + " in group " + playerGroup;
     }
     else {
@@ -145,8 +177,21 @@ memberTable.depthFirst().grep({"tr".equalsIgnoreCase(it.name())}).each {
         spent: brickSpent];
 };
 
-brickDataFile.write(JsonOutput.prettyPrint(JsonOutput.toJson(brickData)), "UTF-8");
-historyFile.write(JsonOutput.prettyPrint(JsonOutput.toJson(history)), "UTF-8");
+brickDataFile.write(JsonOutput.prettyPrint(JsonOutput.toJson(brickData)), encoding);
+
+println "HISTORY FILE";
+println JsonOutput.prettyPrint(JsonOutput.toJson(history));
+historyFile.write(JsonOutput.prettyPrint(JsonOutput.toJson(history)), encoding);
+
+def historyS3Obj = new StorageObject(historyFile);
+historyS3Obj.key = historyFileName;
+historyS3Obj.contentType = "applicaton/json";
+historyS3Obj.contentEncoding = encoding;
+
+s3.putObject(config.S3.bucketName, historyS3Obj);
+
+
+dataFolder.deleteDir();
 
 /*
 write out json
