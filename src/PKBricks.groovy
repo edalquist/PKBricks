8cee36b9ce40ab75182b9611f6b4ea02a1ac2b0c
edalquist <eric.dalquist@gmail.com>
2011-07-19 12:43:39 -0500

    Switch away from json-lib to built-in JSON support

--- src/PKBricks.groovy
+++ src/PKBricks.groovy
@@ -1,26 +1,15 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1' )
 @Grab(group='log4j', module='log4j', version='1.2.16' )
-@Grab(group='net.sf.json-lib', module='json-lib', version='2.4', classifier='jdk15')
 @Grab(group='commons-lang', module='commons-lang', version='2.6')
 
 import org.apache.commons.lang.*;
-import net.sf.*;
-import net.sf.json.*;
-import net.sf.json.groovy.*;
+import groovy.json.*;
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.Method.GET
 import static groovyx.net.http.ContentType.TEXT
 import static groovyx.net.http.ContentType.HTML
 import static groovyx.net.http.ContentType.URLENC
 
-
-GJson.enhanceClasses();
-GJson.enhanceString();
-
-/*
-download this weeks json from s3
-if fail load base json
-*/
 
 //Create a JSON parser
 def jsonSlurper = new JsonSlurper();
@@ -32,15 +21,22 @@ cal.set(Calendar.MINUTE, 0);
 cal.set(Calendar.SECOND, 0);
 cal.set(Calendar.MILLISECOND, 0);
 cal.add(Calendar.DAY_OF_YEAR, -1 * Math.max(0, cal.get(Calendar.DAY_OF_WEEK) - 1));
-def brickTimeStamp = cal.getTimeInMillis();
+def brickTimeStamp = cal.getTimeInMillis().toString();
 
-def dataFolder = "/Users/edalquist/java/workspace_37/PKBricks/src/";
-def historyFile = dataFolder + "history.json";
-def brickDefaultsFile = dataFolder + "brickStatusDefaults.json";
+def dataFolder = new File("/Users/edalquist/java/workspace_37/PKBricks/src/");
+def historyFile = new File(dataFolder, "history.json");
+def brickDefaultsFile = new File(dataFolder, "brickStatusDefaults.json");
 
-def history = jsonSlurper.parse("file:" + historyFile);
-def brickDataFilename = history[brickTimeStamp.toString()];
-def brickDataFile = dataFolder + brickDataFilename;
+def history;
+if (historyFile.exists()) {
+    history = jsonSlurper.parseText(historyFile.getText("UTF-8"));
+}
+else {
+    history = [:];
+}
+
+def brickDataFilename = history[brickTimeStamp];
+def brickDataFile;
 def brickData;
 if (brickDataFilename == null) {
     brickDataFilename = "brickStatus_" + 
@@ -48,12 +44,14 @@ if (brickDataFilename == null) {
         StringUtils.leftPad(String.valueOf(cal.get(Calendar.MONTH) + 1), 2, '0') + 
         cal.get(Calendar.DAY_OF_MONTH) + ".json";
 
-    brickDataFile = dataFolder + brickDataFilename;
-    println "No existing brick data in history for " + brickTimeStamp + ". Creating: " + brickDataFilename;
-    brickData = jsonSlurper.parse("file:" + brickDefaultsFile);
+    brickDataFile = new File(dataFolder, brickDataFilename);
+    println "No existing brick data in history for " + brickTimeStamp + ". Using: " + brickDefaultsFile;
+    brickData = jsonSlurper.parseText(brickDefaultsFile.getText("UTF-8"));
+    history[brickTimeStamp] = brickDataFilename;
 }
 else {
-    brickData = jsonSlurper.parse("file:" + brickDataFile);
+    brickDataFile = new File(dataFolder, brickDataFilename);
+    brickData = jsonSlurper.parseText(brickDataFile.getText("UTF-8"));
 }
 
 //Parse out levels to point to group names
@@ -108,7 +106,7 @@ def memberTable = kdProfile.depthFirst().grep({ "table".equalsIgnoreCase(it.name
 //Read each row of table
 def rowCount = 0;
 memberTable.depthFirst().grep({"tr".equalsIgnoreCase(it.name())}).each {
-    //Skip header data 
+    //Skip header rows (not sure why we find two ...) 
     if (rowCount < 2) {
         rowCount++;
         return;
@@ -116,24 +114,28 @@ memberTable.depthFirst().grep({"tr".equalsIgnoreCase(it.name())}).each {
     
     def cells = it.children();
     def firstCell = cells[0].children()[0];
-    def profileUrl = firstCell['@href'].text();
-    def profilePage = http.get(uri : 'http://www.parallelkingdom.com/' + profileUrl);
-    
-    def profileLevel = profilePage.depthFirst().grep({ "span".equalsIgnoreCase(it.name()) && it.@id == "MainContent_ProfileBody_character_level"})[0];
-    
+
     def playerName = firstCell.text().trim();
-    def playerLevel = Integer.parseInt(profileLevel.text());
     def brickEarned = Integer.parseInt(cells[4].text().trim());
     def brickSpent = Integer.parseInt(cells[5].text().trim());
 
-    println playerName + ", " + playerLevel + ", " + brickEarned + ", " + brickSpent;
+    println playerName + ", " + brickEarned + ", " + brickSpent;
     
-    //Determine the group the player is in
+    //See if the player exists in the data file, if not we need to look up their level
     def playerGroup = brickData.playerGroupLookup[playerName];
+    def playerLevel;
     if (playerGroup == null) {
+        def profileUrl = firstCell['@href'].text();
+        def profilePage = http.get(uri : 'http://www.parallelkingdom.com/' + profileUrl);
+        def profileLevel = profilePage.depthFirst().grep({ "span".equalsIgnoreCase(it.name()) && it.@id == "MainContent_ProfileBody_character_level"})[0];
+        playerLevel = Integer.parseInt(profileLevel.text());
+    
         playerGroup = brickGroups[playerLevel];
         brickData.playerGroupLookup[playerName] = playerGroup;
-        println playerName + " in group " + playerGroup;
+        println playerName + ", " + playerLevel + " in group " + playerGroup;
+    }
+    else {
+        playerLevel = brickData.groups[playerGroup][playerName].level;
     }
     
     //Update the data for the player
@@ -143,9 +145,8 @@ memberTable.depthFirst().grep({"tr".equalsIgnoreCase(it.name())}).each {
         spent: brickSpent];
 };
 
-def bdf = new File(brickDataFile);
-bdf.delete();
-bdf << brickData.toString(2);
+brickDataFile.write(JsonOutput.prettyPrint(JsonOutput.toJson(brickData)), "UTF-8");
+historyFile.write(JsonOutput.prettyPrint(JsonOutput.toJson(history)), "UTF-8");
 
 /*
 write out json
